de98c481f162585408c65234f78f8a00f4c0b10a
paularmand <paularmand.verhaegen@gmail.com>
2015-01-03 21:18:27 -0500

    Initial working version of jointjs

--- 1_meteor/duktio/client/views/duks/duks_index.js
+++ 1_meteor/duktio/client/views/duks/duks_index.js
@@ -1,4 +1,41 @@
 Template.duksIndex.rendered = function() {
+  var graph = new joint.dia.Graph;
+
+  var paper = new joint.dia.Paper({
+      el: $('#duktile'),
+      width: 600,
+      height: 200,
+      model: graph,
+      gridSize: 1
+  });
+
+  function new_node(dukt_name) {
+    return new joint.shapes.basic.Rect({
+        position: { x: 100, y: 30 },
+        size: { width: 100, height: 30 },
+        attrs: { rect: { fill: 'blue' }, text: { text: dukt_name, fill: 'white' } }
+    });
+  };
+
+  // Add all dukt nodes individually
+  // TODO: Better just loop over the collection
+  this.$(".dukt_in_overview .dukt_name a").each(function(index) {
+    var node = new_node($(this).text());
+    graph.addCells([node]);
+  })
+
+  // Add all links (edges)
+  // TODO: Better just loop over the collection
+
+  // var link = new joint.dia.Link({
+  //     source: { id: rect.id },
+  //     target: { id: rect2.id }
+  // });
+
+  // Layout
+
+  // Event: Open Editor
+
 };
 
 Template.duksIndex.helpers({
